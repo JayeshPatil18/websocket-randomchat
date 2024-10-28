3c427cf9adb034043b4afe682361011e5e0d8679
JayeshPatil18 <jp7470484@gmail.com>
2024-10-28 23:07:07 +0530

    final commit

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173', // Allow your React app's origin
+    origin: '*',
     methods: ['GET', 'POST'],
     credentials: true, // Allow credentials if needed
   },
